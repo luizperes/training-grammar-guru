5a7c8aeea3d758d41f65586a690975e06795f759
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2018-12-01 18:53:20 -0800

    Test and fix on the lstm

---
+++ test-files/totally-ordinary-wrong.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var $form = $('.form');
+  $form.submit(function(evt) 
+    evt.preventDefault();
+    if (name) {
+      $form.addClass('highlight');
+    }
+
+    $.get('/url', function () {
+      $('body').addClass('done');
+    });
+  });
+});
+
+/*globals $ */
