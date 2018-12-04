2f3f06bbbaa7b2dbe07c832aa74915b71264cf70
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2018-12-03 23:38:39 -0800

    Adding file to find the table info

---
+++ evaluation/find_source_info.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Tables:
+# ----------------------
+# Table meta | language | mined
+# ----------------------
+# Table repository | name | link | hash | date | License
+# ----------------------
+# Table source-file | hash | code
+# ----------------------
+import sqlite3
+import sys
+
+if __name__ == "__main__":
+    dbname = sys.argv[1]
+    con = sqlite3.connect(dbname)
+    cursor = con.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    print("----------------------")
+    for tbl in tables:
+        print("Table " + tbl[0] + " |")
+        cursor.execute("SELECT * FROM "+tbl[0]+";")
+        for description in cursor.description:
+            print(" " + description[0] + " |")
+        print ("----------------------")
