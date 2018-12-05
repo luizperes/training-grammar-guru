6be04500fdf3617d60eae8f0d0be76f58ae7e05f
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2018-12-04 19:52:31 -0800

    random file generator

--- evaluation/random_file.py
+++ evaluation/random_file.py
@@ -1,11 +1,23 @@
-import sqlite3
+import os
 import sys
+import sqlite3
 
-def get_random_file(db):
+FOLDER = 'source'
+
+def get_random_file(db, n):
     con = sqlite3.connect(db)
     cursor = con.cursor()
-    cursor.execute("SELECT hash FROM repository_source ORDER BY RANDOM() LIMIT 1;")
-    row = cursor.fetchall()[0]
-    cursor.execute("SELECT source FROM source_file WHERE hash = \'" + row[0] + "\';")
-    row = cursor.fetchall()[0]
-    return str(row[0])
+    for i in range(int(n)):
+        file = open(FOLDER + "/" + str(i) + '.js', "w") 
+        cursor.execute("SELECT hash FROM repository_source ORDER BY RANDOM() LIMIT 1;")
+        row = cursor.fetchall()[0]
+        cursor.execute("SELECT source FROM source_file WHERE hash = \'" + row[0] + "\';")
+        row = cursor.fetchall()[0]
+        file.write(row[0])
+        print('copied file ' + str(i))
+        file.close()
+
+if __name__ == '__main__':
+    if not os.path.exists(FOLDER):
+        os.makedirs(FOLDER)
+    get_random_file(sys.argv[1], sys.argv[2])
