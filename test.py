5e7b62c7059c04498c20ab35fec35b6f967fa383
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2018-12-05 00:22:32 -0800

    Everybody has MRR 1 yaaaaaay

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ weights_backwards = str(detect.THIS_DIRECTORY / 'javascript-tiny.backwards.5.h5'
 TKOperation = namedtuple('TKOperation',
                         'old_token position op')
 
-def get_common(tokens):
+def get_common(tokens, filename):
     file_vector = detect.vectorize_tokens(tokens)
     forwards_model = detect.Model.from_filenames(architecture=architecture,
                                                  weights=weights_forwards,
@@ -40,11 +40,11 @@ def mutate(fStr):
     operation = None
     new_str = ''
     while valid:
-        op = random.randint(0, 3)
-        tk = detect.id_to_token(random.randint(0, len(vocabulary)))
+        op = random.randint(0, 2)
+        tk = detect.id_to_token(random.randint(0, len(vocabulary) - 2))
         with detect.synthetic_file(fStr) as f:
             tokens = detect.tokenize_file(f)
-        pos = random.randint(0, len(tokens))
+        pos = random.randint(0, len(tokens) - 1)
         operation = TKOperation(tokens[pos], pos, op)
         if (op == 0): # insert token
             tokens.insert(pos, tk)
@@ -59,8 +59,11 @@ def mutate(fStr):
         tokens = detect.tokenize_file(f)
     return (tokens, operation)
 
+def predict(common):
+    return 1
+
 def test(db, n, iter, reset):
-    answers = np.zeros(n)
+    ranks = np.zeros(n)
 
     if reset:
         print('Extracting files from database...')
@@ -73,20 +76,22 @@ def test(db, n, iter, reset):
         for filename in files:
             lst_files.append(filename)
     
-    for file in lst_files:
+    for idx, file in enumerate(lst_files):
         f = open(SOURCE_DIR + '/' + file, 'r')
         fStr = f.read()
         f.close()
-        for _ in range(0, iter):
-            (mutant, operation) = mutate(fStr)
-            print(str(operation))
-            #common = get_common(mutant)
-            #print("file" + file)
         
-
-    # for each file, random tests
-    # calc MRR
-    # calc total
+        print('Calculating MRR ' + file + '...')
+        ranks_file = np.zeros(iter)
+        for i in range(0, iter):
+            (mutant, operation) = mutate(fStr)
+            common = get_common(mutant, file)
+            ranks_file[i] = predict(common)
+        rank_file = detect.mean_reciprocal_rank(ranks_file)
+        ranks[idx] = rank_file
+        print('Rank MRR ' + file + ':', rank_file)
+    
+    print("Final MRR: ", detect.mean_reciprocal_rank(ranks))
 
 # argv[1] Database path
 # argv[2] number of files
