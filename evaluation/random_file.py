034f2416fff68f80796cf56958993bcc0d0a3909
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2018-12-08 00:27:28 -0800

    Making the font alike power point font

--- evaluation/random_file.py
+++ evaluation/random_file.py
@@ -2,6 +2,7 @@ import os
 import sys
 import shutil
 import sqlite3
+import detect
 
 FOLDER = 'source'
 
@@ -9,14 +10,21 @@ def get_random_file(db, n):
     con = sqlite3.connect(db)
     cursor = con.cursor()
     for i in range(int(n)):
-        file = open(FOLDER + "/" + str(i) + '.js', "w") 
-        cursor.execute("SELECT hash FROM repository_source ORDER BY RANDOM() LIMIT 1;")
-        row = cursor.fetchall()[0]
-        cursor.execute("SELECT source FROM source_file WHERE hash = \'" + row[0] + "\';")
-        row = cursor.fetchall()[0]
-        file.write(row[0].decode('UTF-8'))
-        print('copied file ' + str(i))
-        file.close()
+        while True:
+            cursor.execute("SELECT hash FROM repository_source ORDER BY RANDOM() LIMIT 1;")
+            row = cursor.fetchall()[0]
+            cursor.execute("SELECT source FROM source_file WHERE hash = \'" + row[0] + "\';")
+            row = cursor.fetchall()[0]
+            try:
+                with detect.synthetic_file(row[0].decode('UTF-8')) as f:
+                    tokens = detect.tokenize_file(f)
+                file = open(FOLDER + "/" + str(i) + '.js', "w") 
+                file.write(detect.tokens_to_source_code(tokens))
+                print('copied file ' + str(i))
+                file.close()
+                break
+            except:
+                pass
 
 if __name__ == '__main__':
     if not os.path.exists(FOLDER):
